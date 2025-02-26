d5bb578142194cbc132d138987828145c66191da
Dmitry Avgustis <davgustis@gem.cz>
2025-02-26 15:35:08 +0000

    functional setup with nia

--- src/main/java/com/example/springsaml/SecurityConfig.java
+++ src/main/java/com/example/springsaml/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.springsaml;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -12,10 +13,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // Disable security for all endpoints
+                        .anyRequest().authenticated()
                 )
                 .csrf(csrf -> csrf.disable())
-//                .saml2Login(Customizer.withDefaults())
+                .saml2Login(Customizer.withDefaults())
         ;
 
         return http.build();
